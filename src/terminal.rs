e30bee93983b091dd706db10ce9b6aec2c46dfc4
astria <sachaguerin.Sg@gmail.com>
2025-03-04 22:58:27 +0100

    IMPROVE DISPLAY

--- src/terminal.rs
+++ src/terminal.rs
@@ -42,20 +42,38 @@ pub fn track_playing_display(max_time: u64, track_name: &str) {
 
     clear_terminal();
 
-    let mut current_timestamp = 0;
+    let mut current_timestamp: u64 = 0;
 
     while current_timestamp < max_time {
 
-        println!("       {}", track_name);
+        let mut stdout: io::Stdout = stdout();
+        let terminal_size: (u16, u16) = get_term_size();
+        let cols: u16 = terminal_size.0;
+        let rows: u16 = terminal_size.1;
 
+        // println!("{}", track_name);
+
+        let col_to_print = cols/2 - track_name.len() as u16/2 ;
+
+        execute!(
+            stdout,
+            MoveTo(col_to_print, rows/2),
+            Print(track_name)
+        ).unwrap();
 
         let percent:f64 = current_timestamp as f64 / max_time as f64 * 100.0;
         let mut cursor: f64 = 0.0;
+        let mut to_print: String = String::new();
 
-        print!("{} |", get_duration_from_int(current_timestamp));
+        // print!("{} |", get_duration_from_int(current_timestamp));
+
+        to_print.push_str(&get_duration_from_int(current_timestamp));
+        to_print.push_str(" |");
 
         while cursor < percent/10.0 {
-            print!(" █");
+            // print!(" █");
+            // to_print.push_str(" █");
+            to_print.push_str(" #");
             cursor += 1.0;
         }
 
@@ -64,13 +82,32 @@ pub fn track_playing_display(max_time: u64, track_name: &str) {
         let mut cursor: f64 = 0.0;
 
         while cursor < rest {
-
-            print!(" ■");
+            // print!(" ■");
+            // to_print.push_str(" ■");
+            to_print.push_str(" -");
             cursor += 1.0;
 
         }
 
-        println!(" | {}", get_duration_display(track_name));
+        // println!(" | {}", get_duration_display(track_name));
+        to_print.push_str("| ");
+        to_print.push_str(&get_duration_display(track_name));
+
+        to_print.push_str(" ");
+
+        let col_to_print = (cols as f64 / 2.0) - (to_print.len() as f64 / 2.0);
+        
+
+        // 00:00 | a a a a a a a a a a a | 00:00 -> 37 char
+
+        execute!(
+            stdout,
+            // MoveTo(cols / 2 - (37 / 2), rows/2+1),
+            MoveTo(col_to_print as u16, rows/2+1),
+            Print(to_print)
+        ).unwrap();
+
+        
 
         print_at_bottom(vec!["Press 'q' to exit".to_string()]);
 
@@ -84,6 +121,12 @@ pub fn track_playing_display(max_time: u64, track_name: &str) {
 
 }
 
+pub fn get_term_size() -> (u16, u16) {
+
+    return crossterm::terminal::size().unwrap();
+
+}
+
 pub fn track_list_display(files: Vec<String>) -> i32 {
 
     let mut action_btn: bool = false;
@@ -92,14 +135,21 @@ pub fn track_list_display(files: Vec<String>) -> i32 {
     loop {
         let files: Vec<String> = files.clone();
         let files_clone: Vec<String> = files.clone();
+        let files_len = files.len();
 
         let mut stdout: io::Stdout = stdout();
 
         let mut writing_cursor_line: i32 = 0;
 
+        // Printing
         for file in files {
             // println!("{}", file);
 
+            let terminal_size = get_term_size();
+            let cols = terminal_size.0;
+            let rows = terminal_size.1;
+            let row_to_print = rows / 2 + writing_cursor_line as u16 - files_len as u16;
+            let col_to_print = cols / 2 - file.len() as u16 / 2;
             let mut to_print: String = String::new();
             to_print.push_str(&file);
             to_print.push_str("\n");
@@ -107,6 +157,7 @@ pub fn track_list_display(files: Vec<String>) -> i32 {
             if cursor_line == writing_cursor_line {
                 execute!(
                     stdout,
+                    MoveTo(col_to_print, row_to_print),
                     SetForegroundColor(Color::Black),
                     SetBackgroundColor(Color::White),
                     Print(to_print),
@@ -115,6 +166,7 @@ pub fn track_list_display(files: Vec<String>) -> i32 {
             } else {
                 execute!(
                     stdout,
+                    MoveTo(col_to_print, row_to_print),
                     SetForegroundColor(Color::White),
                     SetBackgroundColor(Color::Black),
                     Print(to_print),
@@ -125,35 +177,16 @@ pub fn track_list_display(files: Vec<String>) -> i32 {
             writing_cursor_line += 1;
         }
 
-        println!("");
+        print_at_bottom(vec!["Press 'Ctrl+c' to exit".to_string()]);
 
-        if cursor_line == writing_cursor_line {
-            execute!(
-                stdout,
-                SetForegroundColor(Color::Black),
-                SetBackgroundColor(Color::White),
-                Print("Exit"),
-                ResetColor
-            ).unwrap();
-        } else {
-            execute!(
-                stdout,
-                SetForegroundColor(Color::White),
-                SetBackgroundColor(Color::Black),
-                Print("Exit"),
-                ResetColor,
-            ).unwrap();
-        }
-
-        print_at_bottom(vec!["Salut".to_string(), "Comment".to_string()]);
-
+        // Pointing
         loop {
             let files: Vec<String> = files_clone.clone();
             if event::poll(std::time::Duration::from_millis(100)).unwrap() {
                 if let Event::Key(KeyEvent { code, kind, .. }) = event::read().unwrap() {
     
                     if code == KeyCode::Down && kind == KeyEventKind::Press {
-                        if cursor_line < files.len() as i32{
+                        if cursor_line < files.len() as i32 - 1{
                             cursor_line += 1;
                         }
                     } else if code == KeyCode::Up && kind == KeyEventKind::Press {
